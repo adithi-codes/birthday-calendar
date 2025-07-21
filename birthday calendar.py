6247e4ebce885112261253f0af71d814cc197a88
Adithi <aditivenkat3@gmail.com>
2025-07-21 00:30:12 -0700

    Add files via upload

---
+++ birthday calendar.py
@@ -0,0 +1,14 @@
+#BIRTHDAY CALENDAR
+name=input("My name is:")
+age=int(input("I am currently:"))
+Birth_year=int(input("I was born on:"))
+Current_year=int(input("It is currently:"))
+age=age+1
+print(f"I am finally {age} years old")
+if age < 13:
+    print("OH, i am still a child")
+elif age<18:
+    print("WOW, i am a teenager now")
+else :
+    print("omg, finally i am an adult now")
+print("HAPPY BIRTHDAY..HAVE A WONDERFUL DAYYY")
